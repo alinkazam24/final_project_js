9b5a0cd1b70ff706a995f696880b1bc17341b0f5
Alina Zamirovskaya <klounano23@example.com>
2026-02-05 22:50:55 +0300

    final project js

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ window.addEventListener('load', ()=>{
 })
 
 
-const subjects =  [
-    // { subject: 'Математика', grades: [1, 1, 2, 3, 1, 1] },
-    // { subject: 'Информатика', grades: [1, 2, 1] },
-    // { subject: 'Чешский язык', grades: [] },
-    // { subject: 'Математика', grades: [1, 1, 2, 3, 1, 1] },
+const mySubjects =  [
+    { subject: 'Математика', grades: [1, 1, 2, 3, 1, 1] },
     { subject: 'Информатика', grades: [1, 2, 1] },
-    { subject: 'Чешский язык', grades: [] }
+    { subject: 'Чешский язык', grades: [] },
+    { subject: 'Английский язык', grades: [] },
+    { subject: 'История', grades: [] },
+    { subject: 'Физика', grades: [] }
+    
 ];
 
 const list = document.querySelector("#list-rangs");
+const selectElement = document.querySelector("#subjects");
+const gradeInput = document.querySelector("#grade-input");
+const addButton = document.querySelector("#add-btn");
+
+
 
 function render(subjects){
-    
+    list.innerHTML = "";
     subjects.forEach(el => {
         const li = document.createElement('li');
         li.classList.add("li");
@@ -35,14 +41,44 @@ function render(subjects){
         }
      
         li.innerHTML = `
-        <span class="">${el.subject}</span>
-        <p class="">${el.grades}, средний бал: ${avr}</p>
+        <span class="subject-name">${el.subject}</span>
+       <p class="grades-info">${el.grades.length > 0 ? el.grades.join(', ') : 'нет оценок'} | Средний балл: ${avr}</p>
+
     `;
     list.appendChild(li);
     });
 };
 
-render(subjects);
+addButton.addEventListener('click', ()=>{
+    const selectedSubject = selectElement.value;
+    const gradeValue = parseInt(gradeInput.value);
 
+    if (isNaN(gradeValue) || gradeValue < 1 || gradeValue > 5) {
+        alert("Пожалуйста, введите корректную оценку от 1 до 5.");
+        return;
+    }
 
+    const subject = mySubjects.find(el => el.subject === selectedSubject);
+    if (subject) {
+        subject.grades.push(gradeValue);
+        render(mySubjects);
+        gradeInput.value = "";
+    }
+    const existingSubject = subjects.find(el => el.subject === selectedSubject);
+    
+    if (existingSubject) {
+       
+        existingSubject.grades.push(gradeValue);
+    } else {
+      
+        subjects.push({
+            subject: selectedSubject,
+            grades: [gradeValue]
+        });
+    }
+    
+    
+    gradeInput.value = '';
+    render(subjects);
+});
 
